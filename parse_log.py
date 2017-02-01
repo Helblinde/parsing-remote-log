efdff0f2abb7a0ea404bdad638c0c2e605fd3acc
Helblinde <helblinde92@gmail.com>
2017-02-01 20:15:42 +0300

    Split of parse_log function into several functions

--- parse_log.py
+++ parse_log.py
@@ -5,6 +5,78 @@ from create_logs import create_logs
 from auth_data import auth_data
 
 LINES_RANGE = 100
+SERVER_LOGS_DIR = '/tmp'
+
+
+def find_server_log(sftp_client, file_pattern):
+    """Auxiliary function for parse_log:
+    Accepts sftp client instance and file pattern;
+    Returns found file name that matches pattern
+    or 'False' if not found
+    """
+    # go to '/tmp' directory with our test server logs
+    sftp_client.chdir(SERVER_LOGS_DIR)
+
+    # finding proper server log with regex
+    for item in sftp_client.listdir('.'):
+        if re.match(file_pattern, item):
+            found_file_name = item
+            break
+    else:
+        # print error message and immediately return False
+        # in case there's no corresponding file among server logs
+        print("Error! Chosen mask doesn't match with any file!")
+        return False
+    return found_file_name
+
+
+def find_line_number(sftp_client, found_file_name, str_id):
+    """Auxiliary function for parse_log:
+    Accepts sftp client instance, found file name and str_id;
+    looks for the specified str_id in any line of the file;
+    Returns line number of found str_id or False otherwise
+    """
+    # seeking for the line number containing specified id
+    line_number = 0
+    with sftp_client.file(found_file_name, 'r') as file:
+        for line in file:
+            line_number += 1
+            if str_id in line:
+                break
+        else:
+            # print error message and immediately return False
+            # in case there's no corresponding id string in log
+            print('Error! str_id is not found inside log')
+            return False
+    return line_number
+
+
+def copy_remote_to_local(sftp_client, found_file_name, line_number):
+    """Auxiliary function for parse_log:
+    Accepts sftp client instance, found file name and line number
+    that contains needed str_id. Copies specified line and +100 and
+    -100 lines of that line from remote file on the server to the
+    "results.txt" file on the local machine where script is called.
+    If line_number is within 100 lines of the beginning or the end
+    of the file, lines will be copied from the beginning or till
+    the end correspondingly, so less than 201 line will be in result
+    """
+    # copy 201 lines totally from server log on the server
+    # to results.txt file in this script's directory
+    with sftp_client.file(found_file_name, 'r') as file_out, \
+            open('results.txt', 'w') as file_in:
+        if line_number >= LINES_RANGE + 1:
+            # skip the beginning of the file in case found line number
+            # is greater than 101 (or LINES_RANGE + 1)
+            # and then start copying
+            for i in range(1, line_number - LINES_RANGE):
+                file_out.readline()
+            for i in range(line_number - LINES_RANGE, line_number + LINES_RANGE + 1):
+                file_in.write(file_out.readline())
+        else:
+            # otherwise simply start copying from the very beginning of the file
+            for i in range(1, line_number + LINES_RANGE + 1):
+                file_in.write(file_out.readline())
 
 
 def parse_log(ip, mask, str_id):
@@ -59,49 +131,22 @@ def parse_log(ip, mask, str_id):
     # copy -100 and +100 lines from that line with str_id
     # including found line itself
     try:
-        # go to '/tmp' directory with our test server logs
-        sftp_client.chdir('/tmp')
-
-        # finding proper server log with regex
-        for item in sftp_client.listdir('.'):
-            if re.match(file_pattern, item):
-                found_file_name = item
-                break
-        else:
-            # print error message and immediately return False
-            # in case there's no corresponding file among server logs
-            print("Error! Chosen mask doesn't match with any file!")
+        # calling 'find_server_log' function to find proper file
+        found_file_name = find_server_log(sftp_client, file_pattern)
+        # return False and immediately exit if nothing found
+        if not found_file_name:
             return False
 
-        # seeking for the line number containing specified id
-        line_number = 0
-        with sftp_client.file(found_file_name, 'r') as file:
-            for line in file:
-                line_number += 1
-                if str_id in line:
-                    break
-            else:
-                # print error message and immediately return False
-                # in case there's no corresponding id string in log
-                print('Error! str_id is not found inside log')
-                return False
+        # calling 'find_line_number' function to find line number containing str_id
+        line_number = find_line_number(sftp_client, found_file_name, str_id)
+        # return False and immediately exit if nothing found
+        if not line_number:
+            return False
 
-        # copy 201 lines totally from server log on the server
-        # to results.txt file in this script's directory
-        with sftp_client.file(found_file_name, 'r') as file_out, \
-                open('results.txt', 'w') as file_in:
-            if line_number >= LINES_RANGE + 1:
-                # skip the beginning of the file in case found line number
-                # is greater than 101 (or LINES_RANGE + 1)
-                # and then start copying
-                for i in range(1, line_number - LINES_RANGE):
-                    file_out.readline()
-                for i in range(line_number - LINES_RANGE, line_number + LINES_RANGE + 1):
-                    file_in.write(file_out.readline())
-            else:
-                # otherwise simply start copying from the very beginning of the file
-                for i in range(1, line_number + LINES_RANGE + 1):
-                    file_in.write(file_out.readline())
+        # calling 'copy_remote_to_local' procedure to copy found file
+        # from remote server to local dir from which script is called
+        copy_remote_to_local(sftp_client, found_file_name, line_number)
+
     finally:
         # close sftp and ssh sessions'
         sftp_client.close()
